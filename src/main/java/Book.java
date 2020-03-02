34efdb4411a4f6dfa82e444969a90a6b16c17cc2
BuildTools <unconfigured@null.spigotmc.org>
2020-03-02 17:04:25 +0300

    Промежуточный результат

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,7 @@
-import java.util.Vector;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 public final class Book {
     public static void main(String[] args){
@@ -9,9 +12,9 @@ public final class Book {
     private String street;
     private int bld;
     private int apt;
-    private static Vector <Book> lst = new Vector <Book> ();
+    private static LinkedList<Book> lst = new LinkedList<>();;
 
-    Book(String name, String street, int bld, int apt) {
+    public Book(String name, String street, int bld, int apt) {
         this.name = name;
         this.street = street;
         this.bld = bld;
@@ -35,11 +38,11 @@ public final class Book {
         return apt;
     }
 
-    public static void addPerson(Book person) {
+    public void addPerson(Book person) {
         lst.add(person);
     }
 
-    public static void removePerson(Book person) {
+    public void removePerson(Book person) {
         lst.remove(person);
     }
 
@@ -52,6 +55,59 @@ public final class Book {
         return "No results found";
     }
 
+    public void changeAdress(String cName, String nStreet, int nBld, int nApt){
+        for (Book it: lst){
+            if (it.name.equals(cName)) {
+                it.street = nStreet;
+                it.bld = nBld;
+                it.apt = nApt;
+            }
+        }
+    }
 
+    public static List<String> findStreet(String fStreet){
+        LinkedList <String> res = new LinkedList<>();
+        for (Book it: lst){
+            if (it.street.equals(fStreet)){
+                res.add(it.name);
+            }
+        }
+        return res;
+    }
+
+    public static List<String> findBld(String fStreet, int fBld){
+        LinkedList <String> res = new LinkedList<>();
+        for (Book it: lst){
+            if (it.street.equals(fStreet) && it.bld == fBld){
+                res.add(it.name);
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", bld=" + bld +
+                ", apt=" + apt +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bld == book.bld &&
+                apt == book.apt &&
+                Objects.equals(name, book.name) &&
+                Objects.equals(street, book.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, street, bld, apt);
+    }
 }
-//Тест
